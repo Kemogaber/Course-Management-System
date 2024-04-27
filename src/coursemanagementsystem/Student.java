9be2d40296d004a586d6cf596c384a6d4a00cccf
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-27 17:52:15 +0300

    Added: addCourse(), dropCourse(), getCourse() and availableCourses.

--- src/coursemanagementsystem/Student.java
+++ src/coursemanagementsystem/Student.java
@@ -4,11 +4,16 @@
  */
 package coursemanagementsystem;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import coursemanagementsystem.courses.Course;
+import coursemanagementsystem.courses.Department;
 
 /**
  *
  * @author Fares Sultan
+ * Student class contians the student's info, and methods to add and drop courses.
  */
 public class Student extends PersonalInformation{
     
@@ -20,11 +25,12 @@ public class Student extends PersonalInformation{
 
     // Student's list of course marks.
     // Includes previously finished courses as well as currently enrolled courses.
-    private List<CourseMark> courseMarks;
-     
+    private List<CourseMark> courseMarks = new ArrayList<CourseMark>();
+    
+    private List<Course> availableCourses = new ArrayList<Course>(); 
 
-    public Student(String name, int id, String passWord, String year, String departement) {
-        super(name, id, passWord, new Department("CSE"));
+    public Student(String name, int id, String passWord, String year, Department departement) {
+        super(name, id, passWord, departement);
         this.year = year;
     }
 
@@ -32,21 +38,89 @@ public class Student extends PersonalInformation{
         return year;
     }
 
+    public List<Course> getAvailableCourses() {
+        return availableCourses;
+    }
+
+    public boolean hasCourse(Course course){
+        for (CourseMark courseMark : courseMarks) {
+            if (courseMark.getCourse() == course) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void updateAvaialableCourses(){
+
+        availableCourses = new ArrayList<Course>();     // Reset the list.
+        for (Course course : department.getCourses()) { // Loop over courses in student's department.
+            if (!this.hasCourse(course)) {              // If the student doesn't have this course.
+                availableCourses.add(course);           // Add it to availableCourses.
+            }
+        }
+    }
+
+    public void displayCourses(){
+        System.out.println(name + "'s courses: ");
+        for (CourseMark courseMark : courseMarks) {
+            System.out.println(courseMark.getCourse().toString());
+        }
+    }
+
+    public CourseMark getCourseMark(Course course) throws Exception{
+
+        if (this.hasCourse(course)) {
+            for (CourseMark courseMark : courseMarks) {
+                if (courseMark.getCourse() == course) {
+                    return courseMark;
+                }
+            }
+        }
+
+        throw new Exception("Student hasn't registered this course.");
+    }
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
         return super.toString() + "\n" + "Year: " + year + "\n--------------------";
     }
     
     @Override
     public void addCourse(Course course) throws Exception {
-        
+
+        // Ensure that the course isn't duplicated.
+        for (CourseMark courseMark : courseMarks) {
+            if (courseMark.getCourse() == course) {
+                throw new Exception("Student already has this course.");
+            }
+        }
+        // Ensure that the course is from the same department,
+        if (course.getDepartment() != this.getDepartment()) {
+            throw new Exception("Student cannot add a course from a different department.");
+        }
+        else {
+            courseMarks.add(new CourseMark(course));
+            this.updateAvaialableCourses();
+        }
     }
 
     @Override
     public void dropCourse(Course course) throws Exception {
+
+        if(!this.hasCourse(course)){    // If course is not registered.
+            throw new Exception("Student hasn't registered this course.");
+        }
         
+        // Else remove courseMark.
+        for (CourseMark courseMark : courseMarks) {
+            if (courseMark.getCourse() == course) {
+                courseMarks.remove(courseMark);
+                this.updateAvaialableCourses();
+                break;
+            }
+        }
+
     }
 
 }

e5c0a51d30c8c1acc0ca508c4af7bcae44414b83
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-29 00:32:05 +0300

    Loads correct fxml.

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException  {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Teacherdashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML.fxml"));
         Scene scene = new Scene(root);
         root.setOnMousePressed((MouseEvent event) -> {
             x = event.getSceneX();

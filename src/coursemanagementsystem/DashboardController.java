eb9df3cdb400b53601199cebc8ed03ff391cb42b
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-28 10:42:58 +0300

    Updates finished courses strings.

--- src/coursemanagementsystem/DashboardController.java
+++ src/coursemanagementsystem/DashboardController.java
@@ -29,167 +29,171 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class DashboardController implements Initializable {
-     @FXML
-    private TableColumn<Course, Integer> CHColumn;
+  @FXML
+  private TableColumn<Course, Integer> CHColumn;
 
-    @FXML
-    private TableColumn<Course, String> CodeColumn;
+  @FXML
+  private TableColumn<Course, String> CodeColumn;
 
-    @FXML
-    private TableColumn<Course, String> CourseNameColumn;
+  @FXML
+  private TableColumn<Course, String> CourseNameColumn;
 
-    @FXML
-    private AnchorPane HomePane;
+  @FXML
+  private AnchorPane HomePane;
 
-    @FXML
-    private AnchorPane RegisterPane;
-    
-    @FXML
-    private Label CHBtn;
+  @FXML
+  private AnchorPane RegisterPane;
 
-    @FXML
-    private Button addBtn;
+  @FXML
+  private Label CHBtn;
 
-    @FXML
-    private Button close;
+  @FXML
+  private Button addBtn;
 
-    @FXML
-    private ChoiceBox<String> combo1;
+  @FXML
+  private Button close;
 
-    @FXML
-    private ChoiceBox<String> combo2;
+  @FXML
+  private ChoiceBox<String> combo1;
 
-    @FXML
-    private Button dropBtn;
+  @FXML
+  private ChoiceBox<String> combo2;
 
-    @FXML
-    private Button home;
+  @FXML
+  private Button dropBtn;
 
-    @FXML
-    private Button outBtn;
+  @FXML
+  private Button home;
 
-    @FXML
-    private Button register;
-    
-    @FXML
-    private Label GpaBtn;
-    
-    @FXML
-    private TableView<Course> tableview1;
-    
-    @FXML
-    private Label CHview;
-    
-        @FXML
-    private Label TotalGpaBtn;
-    
-    String[] Courses={"Digital","Embedded","Programming"};
-    ObservableList<Course> list= FXCollections.observableArrayList(new Course());    
-    Course[] courses= {new Course("Digital",3,"ECE212",2.7),
-            new Course("Embedded",3,"CSE224",3.3),
-            new Course("Programming",2,"CSE231",3.7)};
-    double total=0;double num;double  totGpa;
+  @FXML
+  private Button outBtn;
 
-    @FXML
-    void Closewind(ActionEvent event) {
-         close.getScene().getWindow().hide();
+  @FXML
+  private Button register;
+
+  @FXML
+  private Label GpaBtn;
+
+  @FXML
+  private TableView<Course> tableview1;
+
+  @FXML
+  private Label CHview;
+
+  @FXML
+  private Label TotalGpaBtn;
+
+  ObservableList<Course> list = FXCollections.observableArrayList(new Course());
+  Course[] courses = { new Course("Digital", 3, "ECE212", 2.7),
+      new Course("Embedded", 3, "CSE224", 3.3),
+      new Course("Programming", 2, "CSE231", 3.7) };
+  double total = 0;
+  double num;
+  double totGpa;
+
+  @FXML
+  void Closewind(ActionEvent event) {
+    close.getScene().getWindow().hide();
+  }
+
+  @FXML
+  public void LogOut(ActionEvent event) throws IOException {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Confirmation Message");
+    alert.setHeaderText(null);
+    alert.setContentText("Are you sure you want to logout?");
+    Optional<ButtonType> option = alert.showAndWait();
+    if (option.get().equals(ButtonType.OK)) {
+      outBtn.getScene().getWindow().hide();
+      Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+      Stage stage = new Stage();
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
     }
+  }
 
-    @FXML
-    public void LogOut(ActionEvent event) throws IOException {
-     Alert alert = new Alert(AlertType.CONFIRMATION);
-     alert.setTitle("Confirmation Message");
-     alert.setHeaderText(null);
-     alert.setContentText("Are you sure you want to logout?");
-     Optional<ButtonType> option=alert.showAndWait();
-     if(option.get().equals(ButtonType.OK)){
-         outBtn.getScene().getWindow().hide();
-    Parent root=FXMLLoader.load(getClass().getResource("FXML.fxml"));
-    Stage stage=new Stage();
-    Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
-     }
+  @FXML
+  void Switchform(ActionEvent event) {
+    if (event.getSource() == home) {
+      HomePane.setVisible(true);
+      RegisterPane.setVisible(false);
+    } else if (event.getSource() == register) {
+      HomePane.setVisible(false);
+      RegisterPane.setVisible(true);
+    } else {
+      HomePane.setVisible(false);
+      RegisterPane.setVisible(false);
     }
-    @FXML
-    void Switchform(ActionEvent event)
-    {
-     if(event.getSource()==home){
-         HomePane.setVisible(true);
-         RegisterPane.setVisible(false);
-     }
-     else if (event.getSource()==register){
-          HomePane.setVisible(false);
-         RegisterPane.setVisible(true);
-     }
-     else{
-       HomePane.setVisible(false);
-         RegisterPane.setVisible(false);
-     }
-     }
-      @FXML
-    void DropCourse(ActionEvent event) {
-    Course selecteditem=tableview1.getSelectionModel().getSelectedItem();
-    total-=selecteditem.getCH();
-     for (int i=0;i<courses.length;i++){
-         if (tableview1.getItems().size()!=0){
-         if (tableview1.getSelectionModel().getSelectedItem().getCourseName().equals( courses[i].getCourseName())){
-       num=num-(courses[i].getCH()*courses[i].getGpa());
-       double totGpa=num/total;
-       totGpa=total<=0?0.00:totGpa;
-       totGpa=(total==0 ? 0.00 : totGpa );
-       TotalGpaBtn.setText(String.format("%.2f",totGpa));
-         }
-      } 
+  }
+
+  @FXML
+  void DropCourse(ActionEvent event) {
+    Course selecteditem = tableview1.getSelectionModel().getSelectedItem();
+    total -= selecteditem.getCH();
+    for (int i = 0; i < courses.length; i++) {
+      if (tableview1.getItems().size() != 0) {
+        if (tableview1.getSelectionModel().getSelectedItem().getCourseName().equals(courses[i].getCourseName())) {
+          num = num - (courses[i].getCH() * courses[i].getGpa());
+          double totGpa = num / total;
+          totGpa = total <= 0 ? 0.00 : totGpa;
+          totGpa = (total == 0 ? 0.00 : totGpa);
+          TotalGpaBtn.setText(String.format("%.2f", totGpa));
+        }
+      }
     }
     combo2.getItems().add(tableview1.getSelectionModel().getSelectedItem().getCoursename());
     tableview1.getItems().remove(selecteditem);
     CHview.setText(String.valueOf(total));
 
-    }
-    
-     @FXML
-    void AddCourse(ActionEvent event) {
-      for (int i=0;i<courses.length;i++){
-      if (combo2.getValue().equals( courses[i].getCourseName())){
+  }
+
+  @FXML
+  void AddCourse(ActionEvent event) {
+    for (int i = 0; i < courses.length; i++) {
+      if (combo2.getValue().equals(courses[i].getCourseName())) {
         tableview1.getItems().add(courses[i]);
         combo2.getItems().remove(combo2.getValue());
-        
-      total+=courses[i].getCH();
-      CHview.setText(String.valueOf(total));
-       num+=(courses[i].getCH()*courses[i].getGpa());
-       double totGpa=num/total;
-       TotalGpaBtn.setText(String.format("%.2f",totGpa));
-      } 
+
+        total += courses[i].getCH();
+        CHview.setText(String.valueOf(total));
+        num += (courses[i].getCH() * courses[i].getGpa());
+        double totGpa = num / total;
+        TotalGpaBtn.setText(String.format("%.2f", totGpa));
+      }
     }
-      
-      
-     
-      
+
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    String[] finishedCourses = new String[6];
+
+    for (int i = 0; i < Storage.finishedCourses.length; i++) {
+      finishedCourses[i] = Storage.finishedCourses[i].getCourseName();
     }
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        combo1.setValue(Courses[0]);
-       combo1.getItems().addAll(Courses);
-       combo2.setValue(Courses[0]);
-       combo2.getItems().addAll(Courses);
-       CourseNameColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("coursename"));
-       CHColumn.setCellValueFactory(new PropertyValueFactory<Course,Integer>("CH"));
-       CodeColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("code"));
-       CHview.setText("0");
-       combo1.getSelectionModel().selectedItemProperty().addListener(
-               new ChangeListener(){
-                   @Override
-                   public void changed(ObservableValue observable,Object oldValue, Object newValue)
-                       {
-         for (int i=0;i<combo1.getItems().size();i++){
-      if ((newValue).equals( (Object) courses[i].getCourseName())){
-       GpaBtn.setText(String.format("%.2f",courses[i].getGpa()));
-       CHBtn.setText(String.valueOf(courses[i].getCH()));
+
+    combo1.setValue(Courses[0]);
+    combo1.getItems().addAll(Courses);
+    combo2.setValue(Courses[0]);
+    combo2.getItems().addAll(Courses);
+    CourseNameColumn.setCellValueFactory(new PropertyValueFactory<Course, String>("coursename"));
+    CHColumn.setCellValueFactory(new PropertyValueFactory<Course, Integer>("CH"));
+    CodeColumn.setCellValueFactory(new PropertyValueFactory<Course, String>("code"));
+    CHview.setText("0");
+    combo1.getSelectionModel().selectedItemProperty().addListener(
+        new ChangeListener() {
+          @Override
+          public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            for (int i = 0; i < combo1.getItems().size(); i++) {
+              if ((newValue).equals((Object) finishedCourses[i].getCourseName())) {
+                GpaBtn.setText(String.format("%.2f", finishedCourses[i].getGpa()));
+                CHBtn.setText(String.valueOf(finishedCourses[i].getCH()));
+              }
             }
-       }
-    }});
-  
-    }
-    
-     }
+          }
+        });
+
+  }
+
+}
